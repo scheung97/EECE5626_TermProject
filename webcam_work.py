c85cdc3c560ab854a39c196e6ba96078776df1f8
scheung97 <spendell5@yahoo.com>
2019-08-04 14:06:06 -0400

    added comments for to-do

--- webcam_work.py
+++ webcam_work.py
@@ -22,27 +22,30 @@ while rval:
     if key == ord('c'):
         cv2.imwrite(image, frame)
         print("image captured")
-    elif key == 27:
+    elif key == 27: #key 27 = esc key
         break
-
 vc.release()
-cv2.destroyWindow("preview")
+cv2.destroyAllWindows()
+#TA says to use DLIB, and show qualitative data for report
+    #DLIB frontal face detector doesn't include the forehead and chin (which we want)
 
-time.sleep(5)
+"""perform OpenCV DNN face detection: """
+time.sleep(0)
 output = cv2.imread('testoutput.jpg')
 cv2.imshow('test',output)
 gray = cv2.cvtColor(output,cv2.COLOR_BGR2GRAY) #converts rgb2gray
 cv2.imshow("gray image",gray)
 time.sleep(10)
 
-vc.release()
-cv2.destroyAllWindows()
+#use bounding box dimensions for edge detection
+
+
+
 """-----"""
 
 """
 to do
 #Haar detection --> come up with bounding box --> use sobel detection in bounding box to map face.
-#map face --> find eyes + lips (somehow) --> guesstimate where objects should go based on pixels from features
 
 #facialCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 """
